b6a08871e8a4309dafa217343dfeae0e78e790df
Brett Szalapski <brett_s@apple.com>
2018-11-03 19:15:27 -0700

    added function comments and option to limit total data import

--- src/data_processing/parse_csv.py
+++ src/data_processing/parse_csv.py
@@ -5,7 +5,14 @@ from solar_common.solar_structures import SolarExample, SolarMatrix, SolarLabel
 DATA_FILE = "../data/tract_all.csv"
 
 
-def get_examples_from_csv(csv_path):
+def get_examples_from_csv(csv_path, partial_data=0):
+    """
+    A method to parse the csv data. It assumes that the csv_file has 2 index columns and ditches one of them.
+    It also renames the first column (after removal) to 'index'
+    :param csv_path: The path to the csv file to pull data from
+    :param partial_data: (Optional) The number of examples to use from the dataset
+    :return: SolarMatrix -- a structure containing all of the header names as well as all examples
+    """
     if not os.path.isfile(csv_path):
         print(f"Cannot find the file at {csv_path}")
         raise RuntimeError(f"File {csv_path} not found")
@@ -14,22 +21,26 @@ def get_examples_from_csv(csv_path):
     num_examples = 0
     header = None
 
-    with open(csv_path, 'r') as fp:
+    with open(csv_path, 'r', encoding="ISO-8859-1") as fp:
         for l in fp:
             if not header:
-                header = l.split(',')
+                header = l.split(',')[4:]
+                header[0] = 'index'
+                # print(header)
             else:
-                items = l.split(',')
-                print(items)
+                items = l.split(',')[1:]
+                # print(items)
                 label = SolarLabel(items[3], items[2], items[1])
                 ex_vals = items[4:]
                 ex = SolarExample(ex_vals, label)
                 examples.append(ex)
+                if partial_data > 0 and num_examples > partial_data:
+                    print(f"Opting to not use all data for efficiency. Stopping at {partial_data} examples.")
+                    break
                 num_examples += 1
-                print(label)
-                break
 
     matrix = SolarMatrix(examples, header)
+    print(matrix)
     return matrix
 
 
--- src/solar_common/solar_structures.py
+++ src/solar_common/solar_structures.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 class SolarLabel(object):
+    """
+    A class to contain the labels for the dataset. A label is comprised of:
+    Total Solar Panel Area (per <unknown unit>)
+    Number of Solar Systems (per <unknown unit>)
+    Number of Solar Tiles (per <unknown unit>)
+    """
     area = None
     systems = None
     tiles = None
@@ -12,25 +18,37 @@ class SolarLabel(object):
         self.tiles = tiles
 
     def __str__(self):
-        return f"Label area: {self.area}, System Count: {self.systems}, Tile Count: {self.tiles}"
+        return f"SolarLabel(Area: {self.area}, System Count: {self.systems}, Tile Count: {self.tiles})"
 
     def __repr__(self):
         return f"SolarLabel(area={self.area}, systems={self.systems}, tiles={self.tiles}"
 
 
 class SolarExample(object):
+    """
+    A class that contains all information for a given example.
+    The xs are the pieces of data, and the label is an instance of SolarLabel from above
+    """
     label = None
-    column_vals = None
+    xs = None
 
     def __init__(self, values, label):
-        self.column_vals = np.array(values)
+        self.xs = values
         self.label = label
 
 
 class SolarMatrix(object):
+    """
+    A class to contain the entire set of data.
+    data is an array of SolarExamples
+    headers is an array of strings that label each of the x columns (excluding label columns)
+    """
     data = None
     headers = []
 
     def __init__(self, examples, headers):
         self.data = examples
         self.headers = headers
+
+    def __repr__(self):
+        return f"SolarMatrix(examples: {len(self.data)}. columns: {len(self.headers)}"
