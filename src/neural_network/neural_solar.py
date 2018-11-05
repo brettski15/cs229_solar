8ff473120f91ed70102c79a76cd309c35b879163
Brett Szalapski <brett_s@apple.com>
2018-11-05 12:53:13 -0800

    Refactored model methods to take in separated data. Created method for data separation. Added optional data import limit

--- src/neural_network/neural_solar.py
+++ src/neural_network/neural_solar.py
@@ -1,16 +1,10 @@
-from data_processing.parse_csv import get_examples_from_csv
-
-
-DATA_PATH = "../data/tract_all.csv"
-
-
-def main():
+def main(train_set, valid_set, test_set):
     print("Running NN main")
-    training_set = get_examples_from_csv(DATA_PATH, 1000)
-    print(f"Parsed CSV and found {len(training_set.data)} examples with {len(training_set.labels)} "
-          f"and {len(training_set.headers)} features.")
+
+    print(f"Parsed CSV and found {len(train_set.data)} examples with {len(train_set.labels)} "
+          f"and {len(train_set.headers)} features.")
     # See above for how to access the data, labels, and (if desired) headers
 
 
 if __name__ == '__main__':
-    main()
+    print("Please use `python run.py --nn` to run this model")
--- src/run.py
+++ src/run.py
@@ -1,19 +1,31 @@
 import argparse
 from neural_network.neural_solar import main as nn_main
 from svm.svm_solar import main as svm_main
+from data_processing.parse_csv import get_examples_from_csv, split_data
+
+
+DATA_PATH = "../data/tract_all.csv"
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--svm', action='store_true', help="If specified, run the SVM implementation")
     parser.add_argument('--nn', action='store_true', help="If specified, run the Neural Network implementation")
+    parser.add_argument('--count', '-c', type=int, default=1000, help="The number of pieces of data to use.")
     args = parser.parse_args()
 
+    print(f"Pulling {args.count} examples from the CSV")
+
+    full_data = get_examples_from_csv(DATA_PATH, args.count)
+
+    train_set, valid_set, test_set = split_data(full_data, train_pct=60, valid_pct=20)
+    return
+
     if args.nn:
-        nn_main()
+        nn_main(train_set, valid_set, test_set)
 
     if args.svm:
-        svm_main()
+        svm_main(train_set, valid_set, test_set)
 
 
 if __name__ == '__main__':
