3374e843c323730840b56861fc66870d684c7b9a
Brett Szalapski <brett_s@apple.com>
2018-11-11 20:36:41 -0800

    Using a pandas dataframe for normalization and PCA now

--- src/data_processing/parse_csv.py
+++ src/data_processing/parse_csv.py
@@ -1,4 +1,8 @@
 import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 # from solar_common.solar_structures import SolarExample, SolarMatrix, SolarLabel, SimpleMatrix
 from solar_common.solar_structures import SolarLabel, SimpleMatrix
 
@@ -33,6 +37,58 @@ def split_simple_data(data_matrix, train_pct=60, valid_pct=20):
     return train_set, valid_set, test_set
 
 
+def split_df(data_matrix, label_matrix, train_pct=80, valid_pct=10):
+    """
+    A method for splitting the data and label pandas Dataframes into test, validation, and training sets.
+    Raises a RuntimeError if the total training + validation percents exceed 100 (which would leave no test set)
+    Prints a warning if the test set is less than 5% of the total data.
+
+    :param data_matrix: The Pandas DataFrame of data
+    :param label_matrix: The Pandas DataFrame of labels
+    :param train_pct: The percent of data to use for training. Default: 80%
+    :param valid_pct: The percent of data to use for validation. Default: 10%
+    :return: A tuple of: training_data, training_labels, validation_data, validation_labels, test_data, test_labels
+    """
+    if 100 - train_pct - valid_pct <= 0:
+        raise RuntimeError("Invalid data split--not test data available.")
+
+    if 100 - train_pct - valid_pct < 5:
+        print("!!!!!!!!!!!WARNING: Your test set is less than 5% of the available data. Is that too small?")
+
+    m = len(data_matrix)
+
+    train_count = int((train_pct / 100) * m)
+    valid_count = int((valid_pct / 100) * m)
+
+    frac_train = float(train_pct) / 100
+    frac_valid = float(valid_pct) / 100
+    frac_test = 1.0 - frac_train - frac_valid
+
+    print(f"Putting {train_count} examples in the training set.")
+    print(f"Putting {valid_count} examples in the validation set.")
+    print(f"Leaving {len(data_matrix) - train_count - valid_count} examples in the test set.")
+
+    train_data, x_test_valid, train_labels, label_train_valid = train_test_split(data_matrix, label_matrix,
+                                                                                 test_size=1 - frac_train,
+                                                                                 random_state=0)
+
+    frac_test = frac_test / (frac_valid + frac_test)
+
+    valid_data, test_data, valid_labels, test_labels = train_test_split(x_test_valid, label_train_valid,
+                                                                        test_size=frac_test, random_state=0)
+
+    # train_data = data_matrix[train_count]
+    # train_labels = label_matrix[train_count]
+    #
+    # valid_data = data_matrix[train_count:train_count + valid_count]
+    # valid_labels = label_matrix[train_count:train_count + valid_count]
+    #
+    # test_data = data_matrix[train_count + valid_count:]
+    # test_labels = label_matrix[train_count + valid_count:]
+
+    return train_data, train_labels, valid_data, valid_labels, test_data, test_labels
+
+
 # def split_data(data_matrix, train_pct=60, valid_pct=20):
 #     if 100 - train_pct - valid_pct <= 0:
 #         raise RuntimeError("Invalid data split--not test data available.")
@@ -59,6 +115,50 @@ def split_simple_data(data_matrix, train_pct=60, valid_pct=20):
 #                           data_matrix.headers)
 #
 #     return train_matrix, valid_matrix, test_matrix
+
+
+def get_df_from_csv(csv_path, partial_data=None):
+    """
+    Read 2 pandas DataFrames from the data. The first is all of the data (columns 5-183 plus an index column)
+    The second is the set of label columns (2-4) plus an index column
+    :param csv_path: The path to the csv file to read
+    :param partial_data: [Optional] If set, only use a subset of the available examples
+    :return:
+    """
+    if not os.path.isfile(csv_path):
+        print(f"Cannot find the file at {csv_path}")
+        raise RuntimeError(f"File {csv_path} not found")
+
+    pd.set_option('use_inf_as_na', True)
+
+    with open(csv_path, 'r', encoding='ISO-8859-1') as fp:
+        data_cols = [i for i in range(2, 183)]
+
+        d_matrix = pd.read_csv(fp, nrows=partial_data, usecols=data_cols, na_values=[''],
+                               encoding='ISO-8859-1')
+        # rename_col = d_matrix.columns.values[0]
+        # d_matrix.rename(columns={rename_col: 'idx'}, inplace=True)
+        d_matrix = d_matrix.drop(['county', 'state', 'electricity_price_transportation'], axis=1)
+        d_matrix.replace([np.inf, -np.inf], np.nan)
+        d_matrix.dropna(inplace=True)
+        d_matrix = d_matrix.astype(float, errors='ignore')
+        d_matrix = d_matrix.reset_index()
+        # print(d_matrix)
+
+        labels_matrix = d_matrix.ix[:, 1:4]
+        print(labels_matrix.head())
+        print(d_matrix.head())
+        d_matrix = d_matrix.drop(['tile_count', 'solar_system_count', 'total_panel_area'], axis=1)
+        # print(labels_matrix)
+
+    for row in d_matrix.itertuples(index=True, name='Pandas'):
+        for i in row:
+            if i == np.Inf:
+                print(f"Found string in row. Item: {i}")
+                print(row)
+                continue
+
+    return (d_matrix, labels_matrix)
 
 
 def get_examples_from_csv(csv_path, partial_data=0, ret_simple_matrix=False):
@@ -106,4 +206,5 @@ def get_examples_from_csv(csv_path, partial_data=0, ret_simple_matrix=False):
 
 
 if __name__ == '__main__':
-    get_examples_from_csv(DATA_FILE)
+    # get_examples_from_csv(DATA_FILE)
+    get_df_from_csv(DATA_FILE)
