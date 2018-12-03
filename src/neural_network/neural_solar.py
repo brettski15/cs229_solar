bbff8fe841c6e00aa0847be4b913056be1ab3c85
Eddie Sun <eddiesun@stanford.edu>
2018-12-03 11:23:13 -0800

    updated NN

--- src/neural_network/neural_solar.py
+++ src/neural_network/neural_solar.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from tensorflow import keras as K
+from .neural_plotter import plot_history, plot_test
 
 # https://medium.com/@rajatgupta310198/getting-started-with-neural-network-for-regression-and-tensorflow-58ad3bd75223
 # https://www.kaggle.com/zoupet/neural-network-model-for-house-prices-tensorflow
@@ -13,12 +13,13 @@ def main(train_set, train_labels, valid_set, valid_labels, test_set, test_labels
     # See above for how to access the data and labels
     
     # hyperparameters
-    layer_dims = [64, 64, 1]
-    activations = ['relu', 'relu', 'relu']
+    layer_dims = [256, 128, 32, 1]
+    activations = ['relu', 'relu', 'relu', 'relu']
     lr = 0.001
-    optimizer = tf.train.RMSPropOptimizer(lr)
-    num_epochs = 100
-    batch_size = 64
+    num_epochs = 40
+    batch_size = 128
+    
+    save_path = 'model-and-weights.h5'
     
     # initialize X, Y
     X_train = train_set
@@ -41,19 +42,21 @@ def main(train_set, train_labels, valid_set, valid_labels, test_set, test_labels
     
     # build model
     n_train = X_train.shape[1]
-    model = build_model(layer_dims, activations, optimizer, n_train)
+    model = build_model(layer_dims, activations, n_train, lr)
         
     # train model
     model, history = train_model(model, X_train, Y_train, X_valid, Y_valid, batch_size, num_epochs)
+    model.save(save_path)
     
     # plot train/valid loss (outputs graphs of MSE, MAE, and R2 Coeff.)
-    plot_history(history)
+#    print_stats(history)
+#    plot_history(history)
     
     # predict using model
-    predictions = model.predict(X_test, verbose = 0)
+#    predictions = model.predict(X_test, verbose = 0)
         
     # plot test results (outputs graphs of labels vs. predictions and histogram of prediction error)
-    plot_test(predictions, Y_test)
+#    plot_test(predictions, Y_test)
 
 def convert2np(X_train, Y_train, X_valid, Y_valid, X_test, Y_test):
     
@@ -61,9 +64,9 @@ def convert2np(X_train, Y_train, X_valid, Y_valid, X_test, Y_test):
     X_valid, Y_valid = np.array(X_valid), np.array(Y_valid)
     X_test, Y_test = np.array(X_test), np.array(Y_test)
     
-    X_train = X_train[:, 24:150]
-    X_valid = X_valid[:, 24:150]
-    X_test = X_test[:, 24:150]
+    X_train = X_train[:, :]
+    X_valid = X_valid[:, :]
+    X_test = X_test[:, :]
     
     Y_train = np.reshape(Y_train[:, 0], (Y_train.shape[0], 1))
     Y_valid = np.reshape(Y_valid[:, 0], (Y_valid.shape[0], 1))
@@ -103,7 +106,7 @@ def norm_features(X_train, X_valid, X_test):
 
     return X_train, X_valid, X_test  
 
-def build_model(layer_dims, activations, optimizer, n_train):
+def build_model(layer_dims, activations, n_train, lr):
     
     print ('\n---------------Building NN Model----------------')
     
@@ -118,7 +121,8 @@ def build_model(layer_dims, activations, optimizer, n_train):
         model.add(K.layers.Dense(layer_dims[i]))
         model.add(K.layers.Activation(activations[i]))
 
-    model.compile(loss = 'mse', optimizer = optimizer, metrics = ['mae', r2_keras])
+    model.compile(loss = 'mse', optimizer = K.optimizers.Adam(lr), metrics = ['mae'])
+    # model.compile(loss = 'mse', optimizer = K.optimizers.Adam(lr), metrics = ['mae', r2_keras])
     
     print ('\n------------Building NN Model Complete------------------')
     
@@ -135,6 +139,18 @@ def train_model(model, X_train, Y_train, X_valid, Y_valid, batch_size, num_epoch
     print ('\n-----------Training/Validating NN Model Complete----------------')
     return model, history
 
+def print_stats(history):
+    
+    min_train_loss = min(history.history['loss'])
+    min_val_loss = min(history.history['val_loss'])
+    max_r2_train = max(history.history['r2_keras'])
+    max_r2_val = max(history.history['val_r2_keras'])
+    
+    print('Min Train Loss:', '%.3f' % min_train_loss)
+    print('Min Valid Loss:', '%.3f' % min_val_loss)
+    print('Max Train R2 Coeff.:', '%.3f' % max_r2_train)
+    print('Max Valid R2 Coeff.', '%.3f' % max_r2_val)
+    
 class progress(K.callbacks.Callback):
   def on_epoch_end(self, epoch, logs):
     if epoch % 10 == 0: print('Current Epoch:', epoch)
@@ -142,65 +158,7 @@ class progress(K.callbacks.Callback):
 def r2_keras(y_true, y_pred):
     SS_res =  K.backend.sum(K.backend.square(y_true - y_pred)) 
     SS_tot = K.backend.sum(K.backend.square(y_true - K.backend.mean(y_true))) 
-    return ( 1 - SS_res/(SS_tot + K.backend.epsilon()) )   
-
-def plot_history(history):
-    
-    epoch = history.epoch
-    train_loss = np.array(history.history['loss'])
-    val_loss = np.array(history.history['val_loss'])
-    train_mae = np.array(history.history['mean_absolute_error'])
-    val_mae = np.array(history.history['val_mean_absolute_error'])
-    train_r2 = np.array(history.history['r2_keras'])
-    val_r2 = np.array(history.history['val_r2_keras'])
-    
-    plt.figure(0, figsize=(3.75, 2.75))
-    plt.plot(epoch, train_loss, label = 'Train Loss')
-    plt.plot(epoch, val_loss, label = 'Valid Loss')
-    plt.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss: Mean Squared Error')
-    plt.ylim([0, 500])
-    plt.savefig('fig1.png', bbox_inches="tight")
-
-    plt.figure(1, figsize=(3.75, 2.75))
-    plt.plot(epoch, train_mae, label = 'Train MAE')
-    plt.plot(epoch, val_mae, label = 'Valid MAE')
-    plt.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Absolute Error')
-    plt.savefig('fig2.png', bbox_inches="tight")
-
-    plt.figure(2, figsize=(3.75, 2.75))
-    plt.plot(epoch, train_r2, label = 'Train R2')
-    plt.plot(epoch, val_r2, label = 'Valid R2')
-    plt.legend()
-    plt.ylim([0.5, 1])
-    plt.xlabel('Epoch')
-    plt.ylabel('R2 Coeff')
-    plt.savefig('fig3.png', bbox_inches="tight")
-    
-def plot_test(predictions, Y_test):
-    
-    plt.figure(3, figsize=(3.75, 2.75))
-    plt.plot(Y_test, predictions, 'bo')
-    
-    plt.axis('equal')
-    plt.xlim(plt.xlim())
-    plt.ylim(plt.ylim())
-    plt.gca().set_xlim(left = 0)
-    plt.gca().set_ylim(bottom = 0) 
-    plt.xlabel('Labels')
-    plt.ylabel('Predictions')
-    plt.plot([0, 1000], [0, 1000]) 
-    plt.savefig('fig4.png', bbox_inches="tight")
-    
-    plt.figure(4, figsize=(3.75, 2.75))
-    error = predictions - Y_test
-    plt.hist(error, bins = 10)
-    plt.xlabel("Prediction Error")
-    plt.ylabel("Count") 
-    plt.savefig('fig5.png', bbox_inches="tight")
+    return ( 1 - SS_res/(SS_tot + K.backend.epsilon()) )
     
 if __name__ == '__main__':
     print("Please use `python run.py --nn` to run this model")
