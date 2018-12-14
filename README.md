f5a9ef7ae3c34465dc8a50b5aa33e58f68534302
Brett Szalapski <brett_s@apple.com>
2018-12-13 16:05:14 -0800

    PEP-8 compliance and adding README

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # cs229_solar
+
+Setup:
+------
+1. Make a Python3 virtual environment for use with this project
+2. Run `pip install -r cs229_solar/requirements.txt`
+
+In order to run this submission code, please `cd` into
+the cs229_solar/src directory. From there, execute:
+```bash
+python run.py
+```
+with a selection of options from below:
+
+Neural Network:
+---------------
+```bash
+python run.py --nn -c 0
+```
+Please note that `-c 0` is necessary to force running the code
+on the entire data set. Without this flag, the code will
+default to only 1000 examples in a given run.
+
+Support Vector Machine:
+-----------------------
+```bash
+python run.py --svm -c 0
+```
+
+Primary Component Analysis:
+---------------------------
+```bash
+python run.py --pca -c 0
+```
+Prints out PCA variance contributors, once sorted by PC-1, once by PC-2.
+Also outputs a PCA plot of each of the 3 possible labels to:
+- cs229_solar/output_plots/2_pca_solar_system_count.png
+- cs229_solar/output_plots/2_pca_tile_count.png
+- cs229_solar/output_plots/2_pca_total_panel_area.png
+
+Choropleths (Heatmaps) of data:
+--------------------------------
+```bash
+python run.py --heatmap
+```
+This was used to generate some of the charts included in the
+poster and the write-up. Requires a plotly account.
--- src/svm/svm_solar.py
+++ src/svm/svm_solar.py
@@ -1,111 +1,113 @@
-
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+
 
 def main(train_set, train_labels, valid_set, valid_labels, test_set, test_labels):
 
     # #############################################################################
     # Generate sample data
-    #X_train = np.sort(5 * np.random.rand(40, 1), axis=0)
-    #y_train = np.sin(X).ravel()
+    # x_train = np.sort(5 * np.random.rand(40, 1), axis=0)
+    # y_train = np.sin(X).ravel()
 
     # #############################################################################
     # Add noise to targets
-    #y_train[::5] += 3 * (0.5 - np.random.rand(8))
+    # y_train[::5] += 3 * (0.5 - np.random.rand(8))
 
-    X_train = train_set
-    X_valid = valid_set
-    X_test = test_set
+    x_train = train_set
+    x_valid = valid_set
+    x_test = test_set
 
-    Y_train = train_labels
-    Y_valid = valid_labels
-    Y_test = test_labels
+    y_train = train_labels
+    y_valid = valid_labels
+    y_test = test_labels
 
-    X_train, Y_train, X_valid, Y_valid, X_test, Y_test = convert2np(X_train, Y_train, X_valid, Y_valid, X_test, Y_test)
-    X_train, Y_train = shuffle(X_train, Y_train)
-    X_valid, Y_valid = shuffle(X_valid, Y_valid)
-    X_train, X_valid, X_test = norm_features(X_train, X_valid, X_test)
+    x_train, y_train, x_valid, y_valid, x_test, y_test = convert2np(x_train, y_train, x_valid, y_valid, x_test, y_test)
+    x_train, y_train = shuffle(x_train, y_train)
+    x_valid, y_valid = shuffle(x_valid, y_valid)
+    x_train, x_valid, x_test = norm_features(x_train, x_valid, x_test)
 
-    Y_train = Y_train.T
-    Y_train = Y_train.ravel()
-    Y_valid = Y_valid.T
-    Y_valid = Y_valid.ravel()
-    Y_test = Y_test.T
-    Y_test = Y_test.ravel()
+    y_train = y_train.T
+    y_train = y_train.ravel()
+    y_valid = y_valid.T
+    y_valid = y_valid.ravel()
+    y_test = y_test.T
+    y_test = y_test.ravel()
 
     # #############################################################################
-    #SVR using RBF
+    # SVR using RBF
     print("Fitting SVR using rbf... \n")
-    svr_rbf = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
-    svr_rbf.fit(X_train, Y_train)
-    confidence_rbf_validation = svr_rbf.score(X_valid,Y_valid)
-    confidence_rbf_test = svr_rbf.score(X_test,Y_test)
-    print("Average confidence for validation data using rbf kernel is %f" %confidence_rbf_validation)
-    print("Average confidence for test data using rbf kernel is %f" %confidence_rbf_test)
+    svr_rbf = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='rbf', max_iter=-1,
+                  shrinking=True, tol=0.001, verbose=False)
+    svr_rbf.fit(x_train, y_train)
+    confidence_rbf_validation = svr_rbf.score(x_valid, y_valid)
+    confidence_rbf_test = svr_rbf.score(x_test, y_test)
+    print("Average confidence for validation data using rbf kernel is %f" % confidence_rbf_validation)
+    print("Average confidence for test data using rbf kernel is %f" % confidence_rbf_test)
     print("\n")
     # #############################################################################
     # SVR using Linear
     print("Fitting SVR using Linear... \n")
-    svr_linear = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='linear', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
-    svr_linear.fit(X_train, Y_train)
-    confidence_linear_validation = svr_linear.score(X_valid,Y_valid)
-    confidence_linear_test = svr_linear.score(X_test,Y_test)
-    print("Average confidence for validation data using rbf kernel is %f" %confidence_linear_validation)
-    print("Average confidence for test data using rbf kernel is %f" %confidence_linear_test)
+    svr_linear = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='linear',
+                     max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+    svr_linear.fit(x_train, y_train)
+    confidence_linear_validation = svr_linear.score(x_valid, y_valid)
+    confidence_linear_test = svr_linear.score(x_test, y_test)
+    print("Average confidence for validation data using rbf kernel is %f" % confidence_linear_validation)
+    print("Average confidence for test data using rbf kernel is %f" % confidence_linear_test)
     print("\n")
     # #############################################################################
     # SVR using Poly
     print("Fitting SVR using Poly... \n")
-    svr_poly = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='poly', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
-    svr_poly.fit(X_train, Y_train)
-    confidence_poly_validation = svr_poly.score(X_valid,Y_valid)
-    confidence_poly_test = svr_poly.score(X_test,Y_test)
-    print("Average confidence for validation data using rbf kernel is %f" %confidence_poly_validation)
-    print("Average confidence for test data using rbf kernel is %f" %confidence_poly_test)
+    svr_poly = SVR(C=1e3, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale', kernel='poly', max_iter=-1,
+                   shrinking=True, tol=0.001, verbose=False)
+    svr_poly.fit(x_train, y_train)
+    confidence_poly_validation = svr_poly.score(x_valid, y_valid)
+    confidence_poly_test = svr_poly.score(x_test, y_test)
+    print("Average confidence for validation data using rbf kernel is %f" % confidence_poly_validation)
+    print("Average confidence for test data using rbf kernel is %f" % confidence_poly_test)
     print("\n")
     # #############################################################################
 
-
-    Y_rbf_test_pridict = svr_rbf.predict(X_test)
-    Y_linear_test_pridict = svr_linear.predict(X_test)
-    Y_poly_test_pridict = svr_poly.predict(X_test)
-    print("Y_rbf_test_pridict\n")
-    print(Y_rbf_test_pridict)
+    y_rbf_test_predict = svr_rbf.predict(x_test)
+    y_linear_test_predict = svr_linear.predict(x_test)
+    y_poly_test_predict = svr_poly.predict(x_test)
+    print("y_rbf_test_predict\n")
+    print(y_rbf_test_predict)
     print("=====================================================================")
-    print("Y_linear_test_pridict\n")
-    print(Y_linear_test_pridict)
+    print("y_linear_test_predict\n")
+    print(y_linear_test_predict)
     print("=====================================================================")
-    print("Y_poly_test_pridict\n")
-    print(Y_poly_test_pridict)
+    print("y_poly_test_predict\n")
+    print(y_poly_test_predict)
     print("=====================================================================")
-    print("Y_test\n")
-    print(Y_test)
+    print("y_test\n")
+    print(y_test)
     print("=====================================================================")
 
     # #############################################################################
     # Visualize the prediction result
 
     lw = 2
-    #X_Axis = np.arange(0,76)
-    #X_Axis = np.array([X_Axis])
-    #X_Axis = X_Axis.T
-    #plt.scatter(X_Axis, Y_test, color='black', label='Y_Test_Data')
+    # X_Axis = np.arange(0,76)
+    # X_Axis = np.array([X_Axis])
+    # X_Axis = X_Axis.T
+    # plt.scatter(X_Axis, y_test, color='black', label='Y_Test_Data')
     plt.xlabel("Actual Label")
     plt.ylabel("Predict Label")
     # #############################################################################
     # Plot RBF result
-    #plt.scatter(Y_test, Y_rbf_test_pridict, color='blue', lw=lw, label='RBF Kernel')
-    #plt.title("SVR using RBF Kernel \n Average confidence: %f" %confidence_rbf_test)
+    # plt.scatter(y_test, y_rbf_test_predict, color='blue', lw=lw, label='RBF Kernel')
+    # plt.title("SVR using RBF Kernel \n Average confidence: %f" %confidence_rbf_test)
 
     # #############################################################################
     # Plot Linear result
-    #plt.scatter(Y_test, Y_linear_test_pridict, color='red', lw=lw, label='Linear Kernel')
-    #plt.title("SVR using Linear Kernel \n Average confidence: %f" % confidence_linear_test)
+    # plt.scatter(y_test, y_linear_test_predict, color='red', lw=lw, label='Linear Kernel')
+    # plt.title("SVR using Linear Kernel \n Average confidence: %f" % confidence_linear_test)
 
     # #############################################################################
     # Plot Poly result
-    plt.scatter(Y_test, Y_poly_test_pridict, color='green', lw=lw, label='Polynomial Kernel')
+    plt.scatter(y_test, y_poly_test_predict, color='green', lw=lw, label='Polynomial Kernel')
     plt.title("SVR using Polynomial Kernel \n Average confidence: %f" % confidence_poly_test)
 
     # #############################################################################
@@ -113,78 +115,76 @@ def main(train_set, train_labels, valid_set, valid_labels, test_set, test_labels
     plt.legend()
     plt.show()
 
-    #print(f"Found {len(X.X)} examples with {len(X.labels)} labels.")
+    # print(f"Found {len(X.X)} examples with {len(X.labels)} labels.")
 
-    #train_data = X.X
-    #y = X.labels
-    #train_area = X.get_area_labels()
-    #train_tiles = X.get_tile_count_labels()
-    #train_system = X.get_system_count_labels()
-
-
-
-
-
+    # train_data = X.X
+    # y = X.labels
+    # train_area = X.get_area_labels()
+    # train_tiles = X.get_tile_count_labels()
+    # train_system = X.get_system_count_labels()
 
 
 # #############################################################################
 # Some excellent data manipulation methods
 # Author: Eddie Sun
 
-def convert2np(X_train, Y_train, X_valid, Y_valid, X_test, Y_test):
-    X_train, Y_train = np.array(X_train), np.array(Y_train)
-    X_valid, Y_valid = np.array(X_valid), np.array(Y_valid)
-    X_test, Y_test = np.array(X_test), np.array(Y_test)
+def convert2np(x_train, y_train, x_valid, y_valid, x_test, y_test):
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_valid, y_valid = np.array(x_valid), np.array(y_valid)
+    x_test, y_test = np.array(x_test), np.array(y_test)
 
-    X_train = X_train[:, 24:150]
-    X_valid = X_valid[:, 24:150]
-    X_test = X_test[:, 24:150]
+    x_train = x_train[:, 24:150]
+    x_valid = x_valid[:, 24:150]
+    x_test = x_test[:, 24:150]
 
-    Y_train = np.reshape(Y_train[:, 0], (Y_train.shape[0], 1))
-    Y_valid = np.reshape(Y_valid[:, 0], (Y_valid.shape[0], 1))
-    Y_test = np.reshape(Y_test[:, 0], (Y_test.shape[0], 1))
+    y_train = np.reshape(y_train[:, 0], (y_train.shape[0], 1))
+    y_valid = np.reshape(y_valid[:, 0], (y_valid.shape[0], 1))
+    y_test = np.reshape(y_test[:, 0], (y_test.shape[0], 1))
 
     print('\n---------------SVR Input Shape----------------')
-    print('Train features shape:', X_train.shape)
-    print('Train labels shape:', Y_train.shape)
-    print('Valid features shape:', X_valid.shape)
-    print('Valid labels shape:', Y_valid.shape)
-    print('Test features shape:', X_test.shape)
-    print('Test labels shape:', Y_test.shape)
+    print('Train features shape:', x_train.shape)
+    print('Train labels shape:', y_train.shape)
+    print('Valid features shape:', x_valid.shape)
+    print('Valid labels shape:', y_valid.shape)
+    print('Test features shape:', x_test.shape)
+    print('Test labels shape:', y_test.shape)
     print('--------------------------------------------\n')
 
-    return X_train, Y_train, X_valid, Y_valid, X_test, Y_test
+    return x_train, y_train, x_valid, y_valid, x_test, y_test
+
 
 # #############################################################################
 # Some excellent data manipulation methods
 # Author: Eddie Sun
 
-def shuffle(X, Y):
-    order = np.argsort(np.random.random(X.shape[0]))
-    X = X[order, :]
-    Y = Y[order, :]
+def shuffle(x, y):
+    order = np.argsort(np.random.random(x.shape[0]))
+    x = x[order, :]
+    y = y[order, :]
 
-    return X, Y
+    return x, y
+
 
 # #############################################################################
 # Some excellent data manipulation methods
 # Author: Eddie Sun
 
-def norm_features(X_train, X_valid, X_test):
+def norm_features(x_train, x_valid, x_test):
     print('\n---------------Normalizing Features----------------')
 
-    mean = X_train.mean(axis=0)
-    std = X_train.std(axis=0)
+    mean = x_train.mean(axis=0)
+    std = x_train.std(axis=0)
 
-    X_train = (X_train - mean) / std
-    X_valid = (X_valid - mean) / std
-    X_test = (X_test - mean) / std
+    x_train = (x_train - mean) / std
+    x_valid = (x_valid - mean) / std
+    x_test = (x_test - mean) / std
 
-    print('X_train, X_valid, X_test normalized to zero mean and STD 1.\n')
+    print('x_train, X_valid, X_test normalized to zero mean and STD 1.\n')
 
-    return X_train, X_valid, X_test
+    return x_train, x_valid, x_test
 
 # See above for how to access the data and labels
 
+
 if __name__ == '__main__':
-     print("Please use `python run.py --svm` to run this model")
+    print("Please use `python run.py --svm` to run this model")
